8e08ffd24db6f83387ce6d913b5f6672a2d6223b
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-10 00:52:51 -0700

    Initial Commit

---
+++ ios8_carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios8_carousel
+//
+//  Created by Stanley Ng on 9/10/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
