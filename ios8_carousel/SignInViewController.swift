20ad7ef82da84cd8fff5f2e70288e64c35b34791
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-10 21:06:15 -0700

    add text field

---
+++ ios8_carousel/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  ios8_carousel
+//
+//  Created by Stanley Ng on 9/10/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onBack(sender: UIButton) {
+        println("SignInViewController - onBack")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
